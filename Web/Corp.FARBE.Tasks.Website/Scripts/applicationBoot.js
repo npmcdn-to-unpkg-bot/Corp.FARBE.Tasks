8b6024890392505d258408a06bae7fbc3efcea09
Murat KARATEPE <mrt.karatepe@hotmail.com>
2016-07-19 12:05:59 +0300

    Architecture changes. Required Data, Domain and Queries projects created, AngularJS ng-repeat table test changes applied.

--- Web/Corp.FARBE.Tasks.Website/Scripts/applicationBoot.js
+++ Web/Corp.FARBE.Tasks.Website/Scripts/applicationBoot.js
@@ -6,15 +6,24 @@ tasksApp.controller('taskController',function($scope,$http,$q){
     $scope.testText = "ANGULAR JS WORKED FINE...";
     $scope.getText = "ANGULAR JS WORKED FINE..11.";
 
-    $scope.filter = {
-        street: "ANGULAR JS WORKED FINE...",
-        address: "ANGULAR JS WORKED FINE...",
-        city: "ANGULAR JS WORKED FINE...",
-        zipCode: "ANGULAR JS WORKED FINE...",
-        type: "ANGULAR JS WORKED FINE...",
+    $scope.filters =[{
+        street: "ANGULAR JS ...",
+        address: "ANGULAR JS ...",
+        city: "ANGULAR JS ...",
+        zipCode: "ANGULAR JS ...",
+        type: "ANGULAR JS ...",
         take: 2,
         skip: 0
-    };
+    },
+    {
+        street: "ANGULAR JS ..2222.",
+        address: "ANGULAR JS..2222.",
+        city: "ANGULAR JS .2222..",
+        zipCode: "ANGULAR JS .22222..",
+        type: "ANGULAR JS .22222..",
+        take: 22,
+        skip: 0
+    }];
 
 
 });
